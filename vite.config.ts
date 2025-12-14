7fd56e055adde88f9ab7316cceb15864241e52fa
otpuigsa-art <ot.puigsa@gmail.com>
2025-12-14 17:50:01 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Importante para que las rutas relativas funcionen en GitHub Pages
+  base: '/Engineered-Growth/',
   build: {
     outDir: 'dist',
-  }
-});
+  },
+})
