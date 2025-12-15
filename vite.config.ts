b7ee258565bd9fbc9b24dfa699e117acedb40275
otpuigsa-art <ot.puigsa@gmail.com>
2025-12-15 12:16:24 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',   // 👈 AIXÒ ÉS CLAU
+  base: '/',        // 👈 correcte per domini root
   build: {
     outDir: 'dist',
   },
-});
+})
